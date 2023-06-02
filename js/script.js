54ac4041119dc527ee6e86aa8d70d24df1c06619
LalaSharif <128358802+LalaSharif@users.noreply.github.com>
2023-06-02 12:25:08 +0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function () {
+  $(".workers__profile").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: false,
+    nextArrow: $(".workers__slider_right"),
+  });
+});
+
+function toggleAnswer(id) {
+  var answer = document.getElementById(id);
+  if (answer.style.display === "none") {
+    answer.style.display = "block";
+  } else {
+    answer.style.display = "none";
+  }
+}
